8a75a6e02aa1aa98f82891e920d80949a747e094
Samuel García <samueljose.garcia@iesdonana.org>
2024-01-09 16:25:23 +0100

    Carga inicial

---
+++ database/migrations/2024_01_09_151853_create_notas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('alumno_id')->constrained();
+            $table->foreignId('ccee_id')->constrained();
+            $table->decimal('nota',3, 1);
+            $table->timestamps();
+            $table->unique(['alumno_id', 'ccee_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notas');
+    }
+};
